bcf7da6239f7a4595b87d3bd45cdf5861e552c49
Sandor David Olasz <olaszsandordavid@gmail.com>
2018-02-12 10:23:09 +0100

    1.1

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,6 +1,7 @@
 package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
@@ -9,7 +10,13 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 1000;
+        JsonObject json = request.getAsJsonObject();
+        int minRaise = json.get("minimum_raise").getAsInt();
+        int currentBuyIn = json.get("current_buy_in").getAsInt();
+
+        //String[] json = json.getAsJsonArray()
+
+        return minRaise+currentBuyIn;
     }
 
     public static void showdown(JsonElement game) {
